3f57cf0dc51bfb1d5003374be2ca06bb19777840
Casey Marshall <git@cmars.tech>
2019-03-27 17:56:00 -0500

    Add lint, flake8 fixes, add repo: key to layer.yaml.

--- Makefile
+++ Makefile
@@ -1,2 +1,11 @@
-all:
+all: lint charm/builds/cert-manager
+
+.PHONY: lint
+lint:
+	flake8 --ignore=E121,E123,E126,E226,E24,E704,E265 charm/cert-manager
+
+charm/builds/cert-manager:
 	(cd charm/cert-manager; charm build -o ..)
+
+clean:
+	$(RM) -r charm/builds
--- charm/cert-manager/reactive/cert_manager.py
+++ charm/cert-manager/reactive/cert_manager.py
@@ -2,7 +2,7 @@ import yaml
 
 from charmhelpers.core import hookenv
 
-from charms.reactive import when, when_not, set_flag
+from charms.reactive import when
 from charms.reactive.helpers import data_changed
 
 from charms.layer.tls_client import request_client_cert, request_server_cert
@@ -15,18 +15,22 @@ def config_changed():
         try:
             clients = yaml.loads(hookenv.config().get('clients'))
             for client in clients:
-                request_client_cert(common_name=client['common_name'], sans=client.get('sans', []))
+                request_client_cert(common_name=client['common_name'],
+                                    sans=client.get('sans', []))
         except Exception as e:
             hookenv.log('"clients" config setting is invalid: {}'.format(e))
-            hookenv.status_set('blocked', '"clients" config setting is invalid'
+            hookenv.status_set('blocked',
+                               '"clients" config setting is invalid')
             return
     if data_changed('cert-manager.servers', hookenv.config().get('servers')):
         try:
             servers = yaml.loads(hookenv.config().get('servers'))
             for server in servers:
-                request_server_cert(common_name=server['common_name'], sans=server.get('sans', []))
+                request_server_cert(common_name=server['common_name'],
+                                    sans=server.get('sans', []))
         except Exception as e:
             hookenv.log('"servers" config setting is invalid: {}'.format(e))
-            hookenv.status_set('blocked', '"servers" config setting is invalid'
+            hookenv.status_set('blocked',
+                               '"servers" config setting is invalid')
             return
     hookenv.status_set('active', '')
