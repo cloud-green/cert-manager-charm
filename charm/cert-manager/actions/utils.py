ee0e83c7dcc553e174db7c0351d0f02dc0d6b49c
Casey Marshall <git@cmars.tech>
2019-03-28 12:14:20 -0500

    Using files rather than reactive state to pass content.
    Seems much more reliable that way. Simplified actions.

--- charm/cert-manager/actions/utils.py
+++ charm/cert-manager/actions/utils.py
@@ -1,14 +1,26 @@
-import sys
-
 from charmhelpers.core import hookenv
+
+
+def cert(cn, cert_type):
+    with open('/etc/ssl/cert-manager/{}/{}.crt'.format(cert_type, cn),
+              'r') as f:
+        return f.read()
+
+
+def key(cn, cert_type):
+    with open('/etc/ssl/cert-manager/{}/private/{}.key'.format(cert_type, cn),
+              'r') as f:
+        return f.read()
 
 
 def fail(msg):
     hookenv.action_set({'outcome': 'failure'})
     hookenv.action_fail(msg)
-    sys.exit()
 
 
-def ok(result):
-    hookenv.action_set({'raw', result})
-    hookenv.action_set({'outcome', 'success'})
+def ok_cert(result):
+    hookenv.action_set({'cert': result, 'outcome': 'success'})
+
+
+def ok_certkey(cert, key):
+    hookenv.action_set({'cert': cert, 'key': key, 'outcome': 'success'})
--- charm/cert-manager/reactive/cert_manager.py
+++ charm/cert-manager/reactive/cert_manager.py
@@ -1,11 +1,23 @@
+import os
 import yaml
 
 from charmhelpers.core import hookenv
 
-from charms.reactive import when
+from charms.reactive import when, when_not, set_flag
 from charms.reactive.helpers import data_changed
 
 from charms.layer.tls_client import request_client_cert, request_server_cert
+
+
+@when_not('cert-manager.installed')
+def install():
+    os.makedirs('/etc/ssl/cert-manager/clients', exist_ok=True)
+    os.makedirs('/etc/ssl/cert-manager/clients/private', mode=0o700,
+                exist_ok=True)
+    os.makedirs('/etc/ssl/cert-manager/servers', exist_ok=True)
+    os.makedirs('/etc/ssl/cert-manager/servers/private', mode=0o700,
+                exist_ok=True)
+    set_flag('cert-manager.installed')
 
 
 @when('certificates.available')
@@ -15,22 +27,38 @@ def config_changed():
         try:
             clients = yaml.load(hookenv.config().get('clients'))
             for client in clients:
-                request_client_cert(common_name=client['common_name'],
-                                    sans=client.get('sans', []))
+                cn = client['common_name']
+                hookenv.log('requesting client cert for {}'.format(cn))
+                request_client_cert(cn, client.get('sans', []),
+                                    crt_path='/etc/ssl/cert-manager/clients/'
+                                             '{}.crt'.format(cn),
+                                    key_path='/etc/ssl/cert-manager/clients/'
+                                             'private/{}.key'.format(cn))
         except Exception as e:
             hookenv.log('"clients" config setting is invalid: {}'.format(e))
             hookenv.status_set('blocked',
                                '"clients" config setting is invalid')
             return
+    else:
+        clients = []
     if data_changed('cert-manager.servers', hookenv.config().get('servers')):
         try:
             servers = yaml.load(hookenv.config().get('servers'))
             for server in servers:
-                request_server_cert(common_name=server['common_name'],
-                                    sans=server.get('sans', []))
+                cn = server['common_name']
+                hookenv.log('requesting server cert for {}'.format(cn))
+                request_server_cert(cn, server.get('sans', []),
+                                    crt_path='/etc/ssl/cert-manager/servers/'
+                                             '{}.crt'.format(cn),
+                                    key_path='/etc/ssl/cert-manager/servers/'
+                                             'private/{}.key'.format(cn))
         except Exception as e:
             hookenv.log('"servers" config setting is invalid: {}'.format(e))
             hookenv.status_set('blocked',
                                '"servers" config setting is invalid')
             return
-    hookenv.status_set('active', '')
+    else:
+        servers = []
+    hookenv.status_set('active',
+                       '{} client, '
+                       '{} server certs'.format(len(clients), len(servers)))
